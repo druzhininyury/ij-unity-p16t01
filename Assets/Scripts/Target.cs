c2029e44307859f5c33a81b9a4d87873216641bc
Druzhinin Yury <druzhinin.yy@gmail.com>
2026-02-04 23:18:00 +0300

    Codestyle fixed: gizmos.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,34 +9,9 @@ public class Target : MonoBehaviour
 
     private readonly float _angleSpeed = 135.0f;
     private readonly float _moveSpeed = 3.0f;
-    private readonly Vector3[] _gizmoRoutePointMarker = new[]
-    {
-        new Vector3(-0.5f, 0, 0),
-        new Vector3(0.5f, 0, 0),
-        new Vector3(0, 0, 0.5f),
-        new Vector3(0, 0, -0.5f)
-    };
+    private readonly float _gizmosRoutePointMarkerRadius = 0.4f;
 
     private int _currentRoutePointIndex = 0;
-
-    private void OnDrawGizmosSelected()
-    {
-        if (_routePoints == null || _routePoints.Count == 0)
-        {
-            return;
-        }
-
-        foreach (Vector3 routePoint in _routePoints)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(
-                _gizmoRoutePointMarker[0] + routePoint, 
-                _gizmoRoutePointMarker[1] + routePoint);
-            Gizmos.DrawLine(
-                _gizmoRoutePointMarker[2] + routePoint,
-                _gizmoRoutePointMarker[3] + routePoint);
-        }
-    }
 
     private void Awake()
     {
@@ -59,6 +34,20 @@ public class Target : MonoBehaviour
         if (transform.position == _routePoints[_currentRoutePointIndex])
         {
             _currentRoutePointIndex = (_currentRoutePointIndex + 1) % _routePoints.Count;
+        }
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        if (_routePoints == null || _routePoints.Count == 0)
+        {
+            return;
+        }
+
+        foreach (Vector3 routePoint in _routePoints)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(routePoint, _gizmosRoutePointMarkerRadius);
         }
     }
 }
