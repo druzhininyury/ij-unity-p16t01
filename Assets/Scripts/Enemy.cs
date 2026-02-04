71c457e79a4fb5f484bd02020d027adb440b00db
Druzhinin Yury <druzhinin.yy@gmail.com>
2026-02-04 23:12:15 +0300

    Codestyle fixed: properties.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 2.0f;
-    
-    public Transform target { get; set; }
+
+    public Transform _target;
 
     private void Update()
     {
-        Vector3 targetPosition = target.position;
+        Vector3 targetPosition = _target.position;
         targetPosition.y = transform.position.y;
-        Vector3 moveDirection = (targetPosition - transform.position).normalized;
-        transform.position += _speed * Time.deltaTime * moveDirection;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
     }
 }
